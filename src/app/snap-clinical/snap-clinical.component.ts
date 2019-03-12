721a9ec5fb65e3e27a2ec09745f2a5814c29631b
ABCDuong <stevenduong22@gmail.com>
2019-03-12 01:24:54 -0700

    snapClinical page

--- src/app/snap-clinical/snap-clinical.component.ts
+++ src/app/snap-clinical/snap-clinical.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-snap-clinical',
   templateUrl: './snap-clinical.component.html',
   styleUrls: ['./snap-clinical.component.scss']
 })
-export class SnapClinicalComponent implements OnInit {
+export class SnapClinicalComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#EFEFEB';
   }
 
 }
